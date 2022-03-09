db3d3d9eda0dbbef3f838ffbab07282f550009f2
21bca65 <96609180+21bca65@users.noreply.github.com>
2022-03-09 10:35:14 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+n=int(input("enter number:"))
+temp=n
+rev=0
+while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n=n/10
+if(temp==rev):
+    print(" the number is palindrome")
+else:
+    print(" number is not palindrome")
+    
