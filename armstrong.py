e0b7a38b2331fd13b7cea771770df207936c59c4
21bca65 <96609180+21bca65@users.noreply.github.com>
2022-03-09 10:33:26 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,12 @@
+num=int(input("enter a number:"))
+sum=0
+temp=num
+while temp>0:
+    digit=temp%10
+    sum+=digit**3
+    temp//=10
+if num==sum:
+     print(num,"is an armstrong")
+else:
+    print(num,"is not a armstrong")
+    
