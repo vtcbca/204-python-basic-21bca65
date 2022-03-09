7a6112d8514be3a4099eb3f8d649ae78d5406a92
21bca65 <96609180+21bca65@users.noreply.github.com>
2022-03-09 11:14:07 +0530

    Add files via upload

---
+++ max.py
@@ -0,0 +1,10 @@
+a=int(input("enter 1 value:"))
+b=int(input("enter 2 value:"))
+c=int(input("enter 3 value:"))
+print("maximum:",end='')
+if b<=a>=c:
+    print(a)
+elif a<=b >=c:
+    print(b)
+elif a<=c >=b:
+    print(c)
